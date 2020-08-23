47b258f9c768ff67ca88279168d93e444bff550e
n4hamilt0010 <nathan.hamilton.7d0@gmail.com>
2020-08-23 00:01:48 -0400

    Committing most recent changes before, pulling from master branch and pushing to Secondary-branch.

--- sublets/urls.py
+++ sublets/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('<int:listing_id>/', views.details, name="details"),
     path('<int:listing_id>/subtenantInfo/', views.subtenantInfo, name="subtenantInfo"),
+    path('int:listing_id/subtenantInfo/Generic_Sublet_Agreement_11th.pdf', views.pdf_view,
+         name="Generic_Sublet_Agreement_11th.pdf"),
 
 ]
--- sublets/views.py
+++ sublets/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from sublets.models import SubletListing
 
+from django.http import HttpResponse
+
 
 def index(request):
     listing = SubletListing.objects.select_related('sublet_place', 'sublet_gender', 'sublet_legal_fee',
@@ -22,3 +24,11 @@ def details(request, listing_id):
 
 def subtenantInfo(request, listing_id):
     return render(request, 'sublets/subtenantInfo.html')
+
+
+def pdf_view(request, listing_id):
+    with open('templates/Generic_Sublet_Agreement_11th.pdf', 'rb') as pdf:
+        response = HttpResponse(pdf.read(), mimetype='application/pdf')
+        response['Content-Disposition'] = 'inline;filename=some_file.pdf'
+        return response
+    pdf.closed
