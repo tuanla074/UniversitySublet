3faa56f74a4b6885341dd866df3ca63c9669a00a
Tuan Le <tuan.la074@gmail.com>
2020-08-27 00:21:21 -0400

    Update filters.py add email and phone to contract.html

--- sublets/filters.py
+++ sublets/filters.py
@@ -1,11 +1,18 @@
 import django_filters
-from django_filters import NumberFilter, DateTimeFilter
+from django import forms
+from django_filters import NumberFilter, DateTimeFilter, filters
 from .models import *
 
 
+class DateInput(forms.DateTimeInput):
+    input_type = 'date'
+
+
 class SubletFilter(django_filters.FilterSet):
-    start_date = DateTimeFilter(field_name="sublet_start_date", lookup_expr='gte', label=' start date')
-    end_date = DateTimeFilter(field_name="sublet_end_date", lookup_expr='lte', label='end date')
+    start_date = DateTimeFilter(field_name="sublet_start_date", lookup_expr='gte', label=' start date',
+                                widget=DateInput({'type': 'month'}))
+    end_date = DateTimeFilter(field_name="sublet_end_date", lookup_expr='lte', label='end date',
+                              widget=DateInput({'type': 'month'}))
     price = NumberFilter(field_name="sublist_price", lookup_expr='lte', label='max rent')
 
     class Meta:
@@ -13,4 +20,4 @@ class SubletFilter(django_filters.FilterSet):
         fields = '__all__'
         exclude = ['sublet_owner_info', 'total_room', 'utilities',
                    'sublet_legal_fee', 'parking_cost', 'sublet_status',
-                   'room_number', 'sublet_start_date',  'sublet_end_date', 'sublist_price']
+                   'room_number', 'sublet_start_date', 'sublet_end_date', 'sublist_price']
